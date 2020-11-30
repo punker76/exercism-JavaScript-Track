95793017a7f1bf6b424cfc42f4197d6e0886ca16
punker76 <punkerat76@gmail.com>
2020-11-30 18:41:13 +0100

    Finish Pangram

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -3,6 +3,32 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPangram = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isPangram = (input) => {
+  if (!input) {
+    return false;
+  }
+
+  var set = new Set(
+    [...input.toUpperCase()].filter(v => { return v.charCodeAt(0) >= 65 && v.charCodeAt(0) <= 90 })
+  );
+  return set.size >= 26;
+};
+
+export const isPangram1st = (input) => {
+  if (!input) {
+    return false;
+  }
+
+  input = input.toUpperCase();
+
+  var set = new Set();
+
+  for (var i = 0; i < input.length; i++) {
+    var char = input.charCodeAt(i);
+    if (char >= 65 && char <= 90) {
+      set.add(input.charAt(i));
+    }
+  }
+
+  return set.size >= 26;
 };
